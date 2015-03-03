580e309c147017614272b1997e7fa4056a8a72a5
Raphael Alampay <raphael.alampay@gmail.com>
2015-03-03 11:03:55 +0800

    profile picture for employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ActiveRecord::Base
+  has_attached_file :profile_picture,
+    styles: {
+      medium: "80x80#",
+      standard: "150x150#",
+      large: "250x250#" },
+    default_url: ":attachment/missing_:style.png"
+  validates_attachment_content_type :profile_picture, content_type: %w(image/jpeg image/jpg image/png image/bmp)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302054507) do
+ActiveRecord::Schema.define(version: 20150303030024) do
 
   create_table "deductions", force: :cascade do |t|
     t.string   "name"
@@ -42,9 +42,13 @@ ActiveRecord::Schema.define(version: 20150302054507) do
     t.string   "tin_number"
     t.string   "identification_number"
     t.date     "date_of_birth"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "employee_type_id"
+    t.string   "profile_picture_file_name"
+    t.string   "profile_picture_content_type"
+    t.integer  "profile_picture_file_size"
+    t.datetime "profile_picture_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
